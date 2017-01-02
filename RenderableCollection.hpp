391d853d33c339038c13912ad97333e1b77a9d2a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-01-02 11:52:46 +0000

    the renderable collection is now a shape collection and the entity collection is a renderable collection

--- RenderableCollection.hpp
+++ RenderableCollection.hpp
@@ -4,26 +4,18 @@
 #include <map>
 #include <vector>
 #include "ShapeFactory.hpp"
-#include "Shape.hpp"
+#include "Entity.hpp"
+#include "ShapeCollection.hpp"
 
 class RenderableCollection {
 	protected:
-	unsigned long m_lVerticesSize;
-	unsigned long m_lElementsSize;
-	std::map<ShapeType, GLuint> m_mVertexArrays;
-	std::map<ShapeType, Shape*> m_mShapes;
-	std::map<ShapeType, unsigned long> m_mShapesOffsets;
-	GLuint m_iVertexBuffer;
-	GLuint m_iElementBuffer;
+	std::map<ShapeType, std::vector<Entity*>> m_mEntities;
 
 	public:
-	RenderableCollection() : m_lVerticesSize(0), m_lElementsSize(0) {}
-	void addShape(ShapeType shapeType);
-	void generateBuffers();
-	void generateVertexArrays(GLuint shaderProgram);
-	void clean();
-	void bindVertexArray(ShapeType type);
-	void render(ShapeType type);
+	void addEntity(Entity *entity);
+	void flush();
+	void update();
+	void render(GLuint shaderProgram, ShapeCollection shapes);
 };
 
 #endif
