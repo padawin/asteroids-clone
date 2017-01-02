dc56da691aa73de2fd6077aae5095d86ea7e45b5
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-01-02 15:59:23 +0000

    if an entity can not be updated, free it and remove it from the vector, otherwise, add it to the renderables

--- World.cpp
+++ World.cpp
@@ -7,8 +7,15 @@ void World::addEntity(Entity* entity) {
 void World::update(Vector3D playerPosition) {
 	m_renderables.flush();
 	for (std::vector<Entity*>::size_type i = 0; i < m_vEntities.size(); ++i) {
-		m_vEntities[i]->update(*this, playerPosition);
-		m_renderables.addEntity(m_vEntities.at(i));
+		if (!m_vEntities[i]->update(*this, playerPosition)) {
+			free(m_vEntities[i]);
+			m_vEntities[i] = m_vEntities.back();
+			m_vEntities.pop_back();
+
+		}
+		else {
+			m_renderables.addEntity(m_vEntities.at(i));
+		}
 	}
 }
 
