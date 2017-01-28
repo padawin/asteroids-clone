8e1dde6b2b8a4025a951f516703469d591a1de05
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-01-28 13:51:07 +0000

    parseVertex implemented

--- ObjParser.cpp
+++ ObjParser.cpp
@@ -17,7 +17,7 @@ bool ObjParser::parse(const char* filePath) {
 		}
 		switch (buf[0]) {
 			case 'v':
-				_parseVertex(buf);
+				_parseVertex(vertexIndex, buf);
 				break;
 			case 'f':
 				break;
@@ -29,5 +29,32 @@ bool ObjParser::parse(const char* filePath) {
 	return true;
 }
 
-void ObjParser::_parseVertex(char* line) {
+void ObjParser::_parseVertex(S_VertexIndex &vertexIndex, char* line) {
+	std::stringstream ss(line);
+	switch (line[1]) {
+		// vertex line
+		case ' ':
+			char type[2];
+			float vertexX, vertexY, vertexZ;
+			ss >> type;
+			ss >> vertexX;
+			ss >> vertexY;
+			ss >> vertexZ;
+			if (vertexIndex.indexCoords >= m_vVertices.size()) {
+				S_Vertex vertex;
+				vertex.x = vertexX;
+				vertex.y = vertexY;
+				vertex.z = vertexZ;
+				m_vVertices.push_back(vertex);
+				vertexIndex.indexCoords++;
+			}
+			else {
+				m_vVertices[vertexIndex.indexCoords].x = vertexX;
+				m_vVertices[vertexIndex.indexCoords].y = vertexY;
+				m_vVertices[vertexIndex.indexCoords].z = vertexZ;
+			}
+			break;
+		default:
+			break;
+	}
 }
--- ObjParser.hpp
+++ ObjParser.hpp
@@ -2,12 +2,32 @@
 #define __OBJ_PARSER__
 
 #include <string>
+#include <vector>
 
 #define MAX_CHARS_PER_LINE 512
 
+typedef struct {
+	float x;
+	float y;
+	float z;
+	float r;
+	float g;
+	float b;
+	float textureX;
+	float textureY;
+} S_Vertex;
+
+typedef struct {
+	int indexCoords;
+	int indexTexture;
+	int indexNormal;
+
+} S_VertexIndex;
+
 class ObjParser {
 	private:
-	void _parseVertex(char* line);
+	void _parseVertex(S_VertexIndex &index, char* line);
+	std::vector<S_Vertex> m_vVertices;
 
 	public:
 	bool parse(const char*);
